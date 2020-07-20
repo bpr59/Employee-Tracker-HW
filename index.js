4decc975fe7e44f0b848a6cce5c538f6febf7749
Felix <bpr59@comcast.net>
2020-07-20 12:12:30 -0400

    adding mysql connection and app.listen to index file

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const express =require('express')
 var app = express();
 const bodyparser = require('body-parser');
 
+app.use(bodyparser.json());
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employeeDB',
+    multipleStatements : true
+});
+
+mysqlConnection.connect((err) =>{
+    if(!err)
+    console.log('DB connection active');
+    else
+    console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+})
+
+app.listen(3000,()=>console.log('Express server is listening at port 3000!'));
